6f8854b9f159b60787707932b78a69ae1efa67c2
Vedant2021-hub <82112467+Vedant2021-hub@users.noreply.github.com>
2021-12-28 17:45:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canavs.center;
+}
+
+function draw() {}
+
+function take_snapshot() {
+    save('poseNet.png');
+}
